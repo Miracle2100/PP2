d90f3193356c9c4a40b00aca805d17f41c942ce8
Rustem <destriker9@gmail.com>
2018-01-26 19:02:36 +0600

    second commit

---
+++ week2/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace minmax
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            FileStream newfile = new FileStream(@"C:\test\m.txt", FileMode.Open, FileAccess.Read);//100 , 345 , 444 , 222 , 623, 535 , 54
+            StreamReader sd = new StreamReader(newfile);
+
+            string af = sd.ReadLine();
+
+            string[] num = af.Split(' ', ',');
+            int max = int.Parse(num[0]);
+            int min = int.Parse(num[0]);
+            for (int i = 0; i < num.Length; i++)
+            {
+                 
+                if (int.Parse(num[i]) > max)
+                    max = int.Parse(num[i]);
+                if (int.Parse(num[i]) < min)
+                    min = int.Parse(num[i]);
+            }
+            Console.WriteLine($"min = {min}\nmax = {max}");//54 , 623 
+            Console.ReadKey();
+
+        }
+
+
+
+
+        
+    }
+    }
+
+
+
---
+++ week2/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp6
+{
+
+    
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            for (int j = 2; j < Math.Sqrt(n); j++)
+            
+                if (n % j == 0)
+                    return false;
+                    return true; 
+            
+        }
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream(@"C:\test\t.txt", FileMode.Open, FileAccess.Read);//45 11 7 57 84 100 5
+            StreamReader d = new StreamReader(f);
+           
+            string s = d.ReadLine();
+            f.Close();
+            d.Close();
+            string[] num = s.Split(' ');
+            int min = int.Parse(num[0]);
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                if (min > int.Parse(num[i]) && isPrime(int.Parse(num[i])))
+                    min = int.Parse(num[i]);
+               
+           
+            }
+            Console.WriteLine($"min prime num = {min}");//5
+           
+            FileStream fds  = new FileStream(@"C:\test\t.txt", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fds);
+            sw.WriteLine(min);
+      
+          
+            sw.Close();
+        }
+          
+    }
+    
+}
