2b027e958496766fa4f755bd9b8d8024b934fcfc
Rustem <destriker9@gmail.com>
2018-01-28 22:25:26 +0600

    second commit

---
+++ week2/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStackExample
+{
+    class Program
+    {
+
+        static void showStackTree(string path)
+        {
+            Stack<DirectoryInfo> myS = new Stack<DirectoryInfo>();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            myS.Push(dir);
+
+            while (myS.Count > 0)
+            {
+                DirectoryInfo cur = myS.Pop();
+                foreach (DirectoryInfo d in cur.GetDirectories())
+                {
+                    Console.WriteLine(d.Name);
+                    myS.Push(d);
+                }
+
+                foreach (FileInfo f in cur.GetFiles())
+                {
+                    Console.WriteLine(f.Name);
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            showStackTree(@"C:\test");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{class Complex
+    {
+
+        public int x, y;
+
+
+        public Complex()
+        {
+
+            x = 1;
+            y = 1;
+
+        }
+            public Complex(int a , int b)
+        {
+             x = a;
+             y = b; 
+          }
+            
+        public int GCD( int a , int b)
+        {
+            if (b == 0)
+                return a;
+
+            return GCD(b, a % b);
+
+        }
+        public void abbreviate( )
+        {
+
+            int d = GCD(x, y);
+            x /= d;
+            y /= d;
+
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex n = new Complex();
+            n.y = c1.y * c2.y;
+            n.x = c1.x * n.y / c1.y + c2.x * n.y / c2.y;
+        
+            n.abbreviate();
+            return n;
+
+
+        }
+        public override string ToString()
+        {
+            if (y == 1)
+                return x+".0";
+            else return x + "/" + y;
+        }
+
+
+
+
+    }
+    class program
+    {public static void Main(string[] args)
+        {
+
+            Complex a = new Complex(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Complex b = new Complex(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Complex n = a + b;
+            Console.WriteLine(a + "+" + b + "=" + n);
+            Console.ReadKey();
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
